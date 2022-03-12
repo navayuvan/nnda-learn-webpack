e30a2cd23819f322f5e498296983d3a7df62e8af
Nandha Kumar Viswanathan <nandhakumarviswanaathan@gmail.com>
2022-03-12 12:35:45 +0530

    prettified using prettier...

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import templateFn from './modal.ejs';
 
-$(function(){
-    $('#nnda-btn0').click(function(){
-        $("#nnda-div0").html(templateFn.bind(this, {
-            fname: 'nava',
-            mname: '<b>yuvan</b>',
-            lname: 'viswa',
-            isTrue: () => false
-        }));
-    })
+$(function () {
+    $('#nnda-btn0').click(function () {
+        $('#nnda-div0').html(
+            templateFn.bind(this, {
+                fname: 'nava',
+                mname: '<b>yuvan</b>',
+                lname: 'viswa',
+                isTrue: () => false
+            })
+        );
+    });
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,47 @@
 const path = require('path');
 const webpack = require('webpack');
-const CopyPlugin = require("copy-webpack-plugin");
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     mode: 'development',
-    entry: [
-        './src/index.js'
-    ],
+    entry: ['./src/index.js'],
     plugins: [
         new CopyPlugin({
-            patterns: [{
-                    from: "public/js",
-                    to: "js"
+            patterns: [
+                {
+                    from: 'public/js',
+                    to: 'js'
                 },
                 {
-                    from: "public/css",
-                    to: "css"
+                    from: 'public/css',
+                    to: 'css'
                 },
                 {
-                    from: "public/fonts",
-                    to: "fonts"
+                    from: 'public/fonts',
+                    to: 'fonts'
                 }
-            ],
+            ]
         }),
         new webpack.ProvidePlugin({
-            _: "underscore"
+            _: 'underscore'
         })
     ],
     module: {
-        rules: [{
-            test: /\.ejs$/,
-            loader: 'ejs-loader',
-            options: {
-                variable: 'data'
+        rules: [
+            {
+                test: /\.ejs$/,
+                loader: 'ejs-loader',
+                options: {
+                    variable: 'data'
+                }
             }
-        }]
+        ]
     },
     output: {
         filename: 'js/main.js',
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'dist')
     },
     devServer: {
-        static: './dist',
-    },
+        static: './dist'
+    }
 };
